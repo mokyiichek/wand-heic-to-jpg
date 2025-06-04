63633f0d455e674f840f4d88ca89859d622790b1
Mok Yii Chek <ycgallery@gmail.com>
2025-06-04 16:27:49 +0800

    Fix skipdirs, clarify comment, add newline, add test

--- convert.py
+++ convert.py
@@ -14,32 +14,33 @@ def check_dir(path, file, level=0):
             print("  "*level, "+", entry_path)
 
             if not entry_path in skipdirs:
-                # check directory recursively 
+                # recurse into subdirectory and convert its HEIC files
                 check_dir(os.path.join(path, entry_path), file, level+1)
 
-            # find files in this directory
-            for entry_file in os.listdir(os.path.join(path, entry_path)):
-                if entry_file.endswith(file):
-                    print("  "*(level+1), "-", entry_file)
+                # find files in this directory
+                for entry_file in os.listdir(os.path.join(path, entry_path)):
+                    if entry_file.endswith(file):
+                        print("  "*(level+1), "-", entry_file)
 
-                    file_source = os.path.join(path, entry_path) + "/" + entry_file
-                    file_target = os.path.join(path, entry_path) + "/" + entry_file.replace(".HEIC", ".JPG")
+                        file_source = os.path.join(path, entry_path) + "/" + entry_file
+                        file_target = os.path.join(path, entry_path) + "/" + entry_file.replace(".HEIC", ".JPG")
 
-                    if os.path.isfile(file_target) and os.path.getsize(file_target) > 0:
-                        print("  "*(level+1), " ", "JPG file exists, skipped")
+                        if os.path.isfile(file_target) and os.path.getsize(file_target) > 0:
+                            print("  "*(level+1), " ", "JPG file exists, skipped")
 
-                    else:
-                        print("  "*(level+1), " ", "converting...")
+                        else:
+                            print("  "*(level+1), " ", "converting...")
 
-                        try:
-                            img = Image(filename=file_source)
-                            img.format = 'jpg'
-                            img.save(filename=file_target)
-                            img.close()
-                            print("  "*(level+1), " ", f"saved to {file_target}")
-                        except:
-                            print("  "*(level+1), " ", "HEIC file error, skipped")
+                            try:
+                                img = Image(filename=file_source)
+                                img.format = 'jpg'
+                                img.save(filename=file_target)
+                                img.close()
+                                print("  "*(level+1), " ", f"saved to {file_target}")
+                            except:
+                                print("  "*(level+1), " ", "HEIC file error, skipped")
 
 
-for basepath in basepaths:
-    check_dir(basepath, findfile)
+if __name__ == "__main__":
+    for basepath in basepaths:
+        check_dir(basepath, findfile)
